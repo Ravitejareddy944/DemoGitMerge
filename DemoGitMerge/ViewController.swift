28a601009f0511699d038317c338c057ae881f9a
Ravitejareddy944 <ravitejareddy944@gmail.com>
2024-08-11 13:08:08 +0530

    added register button

--- DemoGitMerge/ViewController.swift
+++ DemoGitMerge/ViewController.swift
@@ -26,9 +26,19 @@ class ViewController: UIViewController {
         button.frame = CGRect(x: 100, y: 600, width: 120, height: 40)
         button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         view.addSubview(button)
+        
+        let registerButton = UIButton()
+        registerButton.setButton(radius: 20, borderWidth: 1.0, borderColor: UIColor.white.cgColor , backgroundcolor: UIColor.red.cgColor)
+        registerButton.setTitle("Register", for: .normal)
+        registerButton.frame = CGRect(x: 100, y: 650, width: 120, height: 40)
+        registerButton.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+        view.addSubview(registerButton)
     }
     
     @objc func buttonTapped() {
+        viewModel.didButtonTapped()
+    }
+    @objc func registerButtonTapped() {
         viewModel.didButtonTapped()
     }
     
